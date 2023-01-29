c2fae5d6a67627c303dfae2567e5c57d4cdd5a41
pseudobunny <ahsnap453@gmail.com>
2023-01-28 23:38:43 -0600

    moved action enum to args (commands will be used for the functions that execute commands)

--- pngme/src/args.rs
+++ pngme/src/args.rs
@@ -1,8 +1,42 @@
-use clap::Parser;
-use crate::commands::Action;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 pub struct Args {
     #[command(subcommand)]
     action: Action
+}
+
+#[derive(Subcommand)]
+pub enum Action {
+    /// Encodes a hidden message in a PNG File
+    Encode {
+        /// Path to PNG file
+        filename: PathBuf,
+        /// Valid PNG spec Chunk Type for identifying the hidden message chunk
+        chunk_type: String,
+        /// Message to Encode
+        message: String,
+        /// File to output message to
+        output: Option<PathBuf>
+    },
+    /// Decodes a hidden message in a PNG File
+    Decode {
+        /// Path to PNG file
+        filename: PathBuf,
+        /// Valid PNG spec Chunk Type for identifying the hidden message chunk
+        chunk_type: String,
+    },
+    /// Removes a hidden message in a PNG File
+    Remove {
+        /// Path to PNG file
+        filename: PathBuf,
+        /// Valid PNG spec Chunk Type for identifying the hidden message chunk
+        chunk_type: String,
+    },
+    /// Prints out the content of a PNG File
+    Print {
+        /// Path to PNG file
+        filename: PathBuf,
+    }
 }
