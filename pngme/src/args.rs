d40269f4a6b3937b78c5289e8df0bd4603fa90a2
pseudobunny <ahsnap453@gmail.com>
2023-01-28 23:33:49 -0600

    chapter 4 of the project

--- pngme/src/args.rs
+++ pngme/src/args.rs
@@ -1,1 +1,8 @@
+use clap::Parser;
+use crate::commands::Action;
 
+#[derive(Parser)]
+pub struct Args {
+    #[command(subcommand)]
+    action: Action
+}
--- pngme/src/commands.rs
+++ pngme/src/commands.rs
@@ -1,1 +1,35 @@
+use std::path::PathBuf;
 
+#[derive(clap::Subcommand)]
+pub enum Action {
+    /// Encodes a hidden message in a PNG File
+    Encode {
+        /// Path to PNG file
+        filename: PathBuf,
+        /// Valid PNG spec Chunk Type for identifying the hidden message chunk
+        chunk_type: String,
+        /// Message to Encode
+        message: String,
+        /// File to output message to
+        output: Option<PathBuf>
+    },
+    /// Decodes a hidden message in a PNG File
+    Decode {
+        /// Path to PNG file
+        filename: PathBuf,
+        /// Valid PNG spec Chunk Type for identifying the hidden message chunk
+        chunk_type: String,
+    },
+    /// Removes a hidden message in a PNG File
+    Remove {
+        /// Path to PNG file
+        filename: PathBuf,
+        /// Valid PNG spec Chunk Type for identifying the hidden message chunk
+        chunk_type: String,
+    },
+    /// Prints out the content of a PNG File
+    Print {
+        /// Path to PNG file
+        filename: PathBuf,
+    }
+}
